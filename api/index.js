66566bbd8bbe9cef232b11de6f723c089450597d
springschool623 <starxuantruong@gmail.com>
2024-07-22 17:12:25 +0700

    seventh-commit

--- api/index.js
+++ api/index.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
@@ -19,7 +21,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/users', async (req,res) => {
+app.get('/user', async (req,res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
