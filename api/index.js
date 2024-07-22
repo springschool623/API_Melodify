5774752ecec1616f6419cddaae3abe656cd7b169
springschool623 <starxuantruong@gmail.com>
2024-07-22 17:09:13 +0700

    sixth-commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
-import User from './model/user';
+import User from './model/user.js';
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,8 +11,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
