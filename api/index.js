538efdaeb50d230749c142f0e4b58a60f2254c9f
springschool623 <starxuantruong@gmail.com>
2024-07-22 16:45:18 +0700

    second commit

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
